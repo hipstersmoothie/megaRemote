b632c5aef688b697b284de715533672d0550c519
Andrew <lisowski54@gmail.com>
2016-11-22 02:05:05 -0800

    header link to '/'

--- src/Views/Header.js
+++ src/Views/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 import './Root.css';
 
 const Header = () => (
-  <div className="App-header" style={{ marginBottom: '15px' }}>
-    <h2>Geek Haus Enterainment Control</h2>
-  </div>
+  <Link to="/" >
+    <div className="App-header" style={{ marginBottom: '15px' }}>
+      <h2>Geek Haus Enterainment Control</h2>
+    </div>
+  </Link>
 );
 
 export default Header;
